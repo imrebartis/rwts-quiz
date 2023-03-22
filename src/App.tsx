2b047ede6fab3fd9e52f49fef77acd962ee59a78
Imre Bartis <imbartis@gmail.com>
2023-03-22 13:13:58 +0200

    get data

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
-import { fetchQuizQuestions, Difficulty } from "./API";
+import { fetchQuizQuestions, Difficulty, QuestionState } from "./API";
 import QuestionCard from "./components/QuestionCard";
 
 const TOTAL_QUESTIONS = 10;
 
 const App = () => {
   const [loading, setLoading] = useState(false);
-  const [questions, setQuestions] = useState([]);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
   const [number, setNumber] = useState(0);
   const [userAnswers, setUserAnswers] = useState([]);
   const [score, setScore] = useState(0);
